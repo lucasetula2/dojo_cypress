5bee62bc52d47b18f866b5005553fe31b2a556d0
Lucas Emanuel Tula <lucasetula@gmail.com>
2022-11-12 18:36:34 -0300

    Agrego xpath, busqueda de google y tarea para casa

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,20 +1,24 @@
-describe("login", () => {
-  it("deberia logearse exitosamente cuenta convex", () => {
-    cy.visit("https://www.login.stagecliengo.com/");
+require('cypress-xpath');
 
-    cy.get("#login-email").type("EderCypress@cliengo.com");
-    cy.get("#login-pass").type("Cliengo123");
-    cy.get('button[type="submit"]').click();
-    //verifica si es visible el modal para cerrarlo
-    cy.get("#react-modal .close").then(($el) => {
-      if (Cypress.dom.isVisible($el)) {
-        cy.get("#react-modal .close").click();
-      }
-    });
-    // verifica el plan de la company mendiante la api de planes
-    cy.planType().should("have.property", "status", 200);
-    cy.planType().then((response) => {
-      expect(response.body).to.have.property("id", "CLIENGO_CONVERSACIONES_CORPORATE"); // true
-    });
+describe("login", () => {
+  it.skip("Busqueda en Google", () => {
+    cy.visit("https://www.google.com.ar/")
+    cy.get('.gLFyf.gsfi').type("enanos") // Utilizo el # si tengo el id del elemento, entonces pongo #id_del_elemento
+    cy.xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]').click({force: true})
+  });
+  
+  it.skip("Busqueda en Youtube", () => {
+    cy.visit("https://www.youtube.com/"); // Entrar en una URL
+
+  });
+
+  it.skip("Login en Facebook", () => {
+    cy.visit("https://www.facebook.com/"); // Entrar en una URL
+
+  });
+
+  it.skip("Login en Instagram", () => {
+    cy.visit("https://www.instagram.com/"); // Entrar en una URL
+
   });
 });
